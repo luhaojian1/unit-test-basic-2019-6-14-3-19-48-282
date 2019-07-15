7141be86e9d6968519da268c9e6429a2165843eb
luhaojian1 <962671114@qq.com>
2019-07-15 15:07:38 +0800

    return expense type A if project is external and name is project A

--- src/test/java/ExpenseService/ExpenseServiceTest.java
+++ src/test/java/ExpenseService/ExpenseServiceTest.java
@@ -5,29 +5,31 @@ import ExpenseService.Expense.ExpenseType;
 import ExpenseService.Project.Project;
 import ExpenseService.Project.ProjectType;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions.*;
 
+import static ExpenseService.Expense.ExpenseType.EXPENSE_TYPE_A;
 import static ExpenseService.Expense.ExpenseType.INTERNAL_PROJECT_EXPENSE;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ExpenseServiceTest {
     @Test
     void should_return_internal_expense_type_if_project_is_internal() throws UnexpectedProjectTypeException {
 
-        Project project = new Project(ProjectType.INTERNAL,"internal");
+        Project project = new Project(ProjectType.INTERNAL, "internal");
 
         ExpenseType expenseType = ExpenseService.getExpenseCodeByProjectTypeAndName(project);
 
-        assertEquals(expenseType,INTERNAL_PROJECT_EXPENSE);
+        assertEquals(expenseType, INTERNAL_PROJECT_EXPENSE);
     }
 
     @Test
     void should_return_expense_type_A_if_project_is_external_and_name_is_project_A() throws UnexpectedProjectTypeException {
-        // given
-        // when
-        // then
+
+        Project project = new Project(ProjectType.EXTERNAL, "Project A");
+
+        ExpenseType expenseType = ExpenseService.getExpenseCodeByProjectTypeAndName(project);
+
+        assertEquals(expenseType, EXPENSE_TYPE_A);
+
     }
 
     @Test
